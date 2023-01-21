9dc025b0803db21ce26f490fe0ada8ba01709e11
Aryanpathak12002417 <94751590+Aryanpathak12002417@users.noreply.github.com>
2023-01-21 23:50:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const skill1=document.querySelector('.skill1');
+const skill2=document.querySelector('.skill2');
+const skill3=document.querySelector('.skill3');
+const skill4=document.querySelector('.skill4');
+const skill5=document.querySelector('.skill5');
+const skill6=document.querySelector('.skill6');
+
+const progress=document.querySelector('.progress-bar')
+
+
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>=1015){
+
+        progress.classList.add('progress-bar-animation-skill1');
+
+        skill1.classList.add('progress-bar-animation-skill1');
+        skill2.classList.add('progress-bar-animation-skill2');
+        skill3.classList.add('progress-bar-animation-skill3');
+        skill4.classList.add('progress-bar-animation-skill4');
+        skill5.classList.add('progress-bar-animation-skill5');
+        skill6.classList.add('progress-bar-animation-skill6');
+
+        
+    }
+})
+
+console.log('YEs')
